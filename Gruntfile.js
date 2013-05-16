34146e4d44b5c0ace0306c446480e43fe7ddc29d
Michael Shick <michael.shick@gmail.com>
2013-05-16 19:21:37 -0400

    removing cssmin -- themes should probably be handled as their own projects. too much to deal with in here

--- Gruntfile.js
+++ Gruntfile.js
@@ -426,7 +426,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default minimal -- only required plugins
   grunt.registerTask("default", ["update_submodules", "build", "pre-uglify", "uglify", "dist:*", "compare_size"]);
